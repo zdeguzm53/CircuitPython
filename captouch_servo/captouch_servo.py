a0f98647e1f1bd35a1f28ceb4269b291ec9ccd66
Zoe B Deguzman <zdeguzm53@ccs.local>
2019-10-18 09:16:38 -0400

    add comments to captouch_servo

--- captouch_servo/captouch_servo.py
+++ captouch_servo/captouch_servo.py
@@ -1,31 +1,31 @@
-import time
-import board
-import pulseio
-import touchio
-import adafruit_motor
-from adafruit_motor import servo
+import time #pylint: disable-msg=import-error
+import board #pylint: disable-msg=import-error
+import pulseio #pylint: disable-msg=import-error
+import touchio #pylint: disable-msg=import-error
+import adafruit_motor #pylint: disable-msg=import-error
+from adafruit_motor import servo #pylint: disable-msg=import-error
 
 pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
-touch_pad = board.A3
-touch_pad2 = board.A5
+touch_pad = board.A3 #pin for first touch wire
+touch_pad2 = board.A5 #pin for second touch wire
 my_servo = servo.Servo(pwm)
-touch = touchio.TouchIn(touch_pad)
+touch = touchio.TouchIn(touch_pad) 
 touch2 = touchio.TouchIn(touch_pad2)
-my_servo.angle = 90
+my_servo.angle = 90 #puts servo in the middle
 
 while True:
 
-    if touch.value:
-        if my_servo.angle > 179.1:
-            my_servo.angle = 1
-        else:
-            my_servo.angle += 1
-        print(my_servo.angle)
+    if touch.value: #if first touch wire is being pushed
+        if my_servo.angle > 179.1: #if it reached 180 degrees
+            my_servo.angle = 1 #go back to 1 degree
+        else: #if it is between 1 and 180 degrees
+            my_servo.angle += 1 #keep moving to the right
+        print(my_servo.angle) #print angle
         time.sleep(0.05)
-    if touch2.value:
-        if my_servo.angle < .9:
-            my_servo.angle = 179
-        else:
-            my_servo.angle -= 1
+    if touch2.value: #if second wire is touched
+        if my_servo.angle < .9: #if it reached 1 degree
+            my_servo.angle = 179 #go back to 179 degrees
+        else: #if it is between 1 and 179
+            my_servo.angle -= 1 #move to the left
         print(my_servo.angle)
         time.sleep(0.05)
