16acd67fca30b38dd4af4b1ee4ed72bebf73bd3c
Zoe B Deguzman <zdeguzm53@ccs.local>
2019-10-21 09:36:28 -0400

    add comments to ultrasonic_sensor

--- ultrasonic_sensor/ultrasonic_sensor.py
+++ ultrasonic_sensor/ultrasonic_sensor.py
@@ -1,37 +1,40 @@
 import time
-import board
-from adafruit_hcsr04 import HCSR04
-import neopixel
+import board #pylint: disable-msg=import-error
+from adafruit_hcsr04 import HCSR04 #pylint: disable-msg=import-error
+import neopixel #pylint: disable-msg=import-error 
 
+ #declares light on the metro
 light = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
+ #declares pins for the sensor
 trig = board.D2
 echo = board.D3
 sonar = HCSR04(trig, echo)
 
+#initial color values
 red = 0
 green = 0
 blue = 0
 
 while True:
-    try:
-        print(int(sonar.distance))
-        print(str(red) + " " + str(green) + " " + str(blue))
+    try: #if the diatance is in range
+        print(int(sonar.distance)) #print distance in cm
+        print(str(red) + " " + str(green) + " " + str(blue)) #print color values
         if int(sonar.distance) < 20:
-            blue = int(sonar.distance)
-            red = 20 - int(sonar.distance)
-            green = 0
+            blue = int(sonar.distance) #blue decreases as distance decreases
+            red = 20 - int(sonar.distance) #red increases as distance decreases
+            green = 0 #light is never green when distance is under 20 cm
         if int(sonar.distance) >= 20 and int(sonar.distance) < 35:
-            green = int(sonar.distance)
-            blue = 35 - int(sonar.distance)
-            red = 0
+            green = int(sonar.distance) #green increases as distance increases
+            blue = 35 - int(sonar.distance) #blue decreases as distance increases
+            red = 0 #never red between 20 and 35 cm
         if int(sonar.distance) >= 35:
-            green = 35
+            green = 35 #always green when distance is over 35 cm
             red = 0
             blue = 0
-        light.fill((red, green, blue))
+        light.fill((red, green, blue)) #turn on led with those color values
         time.sleep(.5)
-    except RuntimeError:
+    except RuntimeError: #when distance is too far away for the sensor
         print("Retrying!")
-        time.sleep(.5)
+        time.sleep(.5) #waits until distance is in range
         pass
