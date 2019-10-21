61725aa60842c05aa0a2e4acfa3932e6ab672bf4
Zoe B Deguzman <zdeguzm53@ccs.local>
2019-10-21 09:23:56 -0400

    add comments to fancyLED class and two_fancy

--- two_fancy/fancyLED.py
+++ two_fancy/fancyLED.py
@@ -3,6 +3,8 @@ import time
 
 class FancyLED:
 
+     #declare object as 3 separate leds
+     #define pin number and that it is an output
      def __init__(self, led1, led2, led3):
          self.LED1 = DigitalInOut(led1)
          self.LED1.direction = Direction.OUTPUT
@@ -11,13 +13,14 @@ class FancyLED:
          self.LED3 = DigitalInOut(led3)
          self.LED3.direction = Direction.OUTPUT
 
+     #slowly switches which led is on
      def alternate(self):
-        for x in range(0, 5):
+        for x in range(0, 5): #loop through 5 times
             print(x)
             self.LED1.value = True
             self.LED2.value = False
             self.LED3.value = False
-            time.sleep(1)
+            time.sleep(1) #stay on for 1 second
             self.LED1.value = False
             self.LED2.value = True
             self.LED3.value = False
@@ -26,9 +29,10 @@ class FancyLED:
             self.LED2.value = False
             self.LED3.value = True
             time.sleep(1)
-          
+
+     #slowly blinks all leds at the same time     
      def blink(self):
-         for x in range(0, 5):
+         for x in range(0, 5): #turn on and off 5 times
             print(x)
             self.LED1.value = True
             self.LED2.value = True
@@ -39,14 +43,14 @@ class FancyLED:
             self.LED3.value = False
             time.sleep(1)
             
-
+     #quickly alternates which led is on
      def chase(self):
-        for x in range(0, 10):
+        for x in range(0, 10): #loops through 10 times
             print(x)
             self.LED1.value = True
             self.LED2.value = False
             self.LED3.value = False
-            time.sleep(.1)
+            time.sleep(.1) #only rests for 1/10th of a second
             self.LED1.value = False
             self.LED2.value = True
             self.LED3.value = False
@@ -56,19 +60,21 @@ class FancyLED:
             self.LED3.value = True
             time.sleep(.1)
 
+     #all leds are off for 3 seconds
      def off(self):
         self.LED1.value = False
         self.LED2.value = False
         self.LED3.value = False
         time.sleep(3)
 
+     #all 3 leds blink very quickly at the same time
      def sparkle(self):
-         for x in range(0, 30):
+         for x in range(0, 30): #blinks 30 times
             print(x)
             self.LED1.value = True
             self.LED2.value = True
             self.LED3.value = True
-            time.sleep(.05)
+            time.sleep(.05) #stays on for 1/20th of a second
             self.LED1.value = False
             self.LED2.value = False
             self.LED3.value = False
--- two_fancy/two_fancy.py
+++ two_fancy/two_fancy.py
@@ -1,7 +1,8 @@
 import board #pylint: disable-msg=import-error
 import time
-from fancyLED import FancyLED
+from fancyLED import FancyLED #imports the class and methods
 
+#led pin numbers
 two = board.D2
 three = board.D3
 four = board.D4
@@ -9,9 +10,12 @@ five = board.D5
 six = board.D6
 seven = board.D7
 
+#defines an object as the class
 fancy1 = FancyLED(two, three, four)
 fancy2 = FancyLED(five, six, seven)
 
+#goes through all the methods
+#turns off for 3 seconds between each one
 while True:
     fancy1.alternate()
     fancy1.off()
