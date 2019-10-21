ddabf5c81c0e0555dbd8f8dadd0a338f66f0a287
Zoe B Deguzman <zdeguzm53@ccs.local>
2019-10-21 09:15:59 -0400

    add comments to rgb class and tester

--- rgb/RGB_tester.py
+++ rgb/RGB_tester.py
@@ -2,6 +2,7 @@ import board #pylint: disable-msg=import-error
 import time
 from rgb import RGB
 
+#pin numbers for each color
 r1 = board.D2
 g1 = board.D3
 b1 = board.D4
@@ -9,9 +10,11 @@ r2 = board.D5
 g2 = board.D6
 b2 = board.D7
 
+#declare objects
 LED1 = RGB(r1,g1,b1)
 LED2 = RGB(r2,g2,b2)
 
+#call methods from class
 LED1.red()
 LED2.green()
 time.sleep(1)
--- rgb/rgb.py
+++ rgb/rgb.py
@@ -1,7 +1,8 @@
-from digitalio import DigitalInOut, Direction
+from digitalio import DigitalInOut, Direction #pylint: disable-msg=import-error
 
 class RGB:
-
+     
+     #declare object with a red pin, green pin, blue pin
      def __init__(self, red, green, blue):
          self.RED_LED = DigitalInOut(red)
          self.RED_LED.direction = Direction.OUTPUT
@@ -10,31 +11,37 @@ class RGB:
          self.BLUE_LED = DigitalInOut(blue)
          self.BLUE_LED.direction = Direction.OUTPUT
 
+     #only red is on
      def red(self):
           self.RED_LED.value = 0
           self.GREEN_LED.value = 255
           self.BLUE_LED.value = 255
 
+     #only green is on
      def green(self):
           self.RED_LED.value = 255
           self.GREEN_LED.value = 0
           self.BLUE_LED.value = 255
 
+     #only blue is on
      def blue(self):
           self.RED_LED.value = 255
           self.GREEN_LED.value = 255
           self.BLUE_LED.value = 0
 
+     #only red and green are on
      def yellow(self):
           self.RED_LED.value = 0
           self.GREEN_LED.value = 0
           self.BLUE_LED.value = 255
 
+     #only red and blue are on
      def magenta(self):
           self.RED_LED.value = 0
           self.GREEN_LED.value = 255
           self.BLUE_LED.value = 0
 
+     #only blue and green are on
      def cyan(self):
           self.RED_LED.value = 255
           self.GREEN_LED.value = 0
